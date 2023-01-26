ff827fd0ceae4c75a556dad3ea66747d8c499cf3
stapa <stapa@me.com>
2023-01-26 18:23:20 +0300

    add Example005_ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Input name user: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "olga")
+{
+    Console.WriteLine("Ура, это же Ольга!");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username); 
+}
